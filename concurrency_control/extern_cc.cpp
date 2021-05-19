0692cbccd9c86a10e04d3cbf5937fec601d75dca
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2021-05-19 00:42:49 -0400

    Merge EXTERN_CC implementation files

--- concurrency_control/extern_cc.cpp
+++ concurrency_control/extern_cc.cpp
@@ -1,5 +1,3 @@
-/* Simple implementation of register_txn (uses single global lock). */
-
 #include <pthread.h>
 
 #include <cstddef>
@@ -46,10 +44,33 @@ void* lock_and_commit(void* arg) {
     return NULL;
 }
 
-#if EXTERN_CC_IMPL == EXTERN_CC_GL
-
 UInt32 g_extern_thread_cnt = 0;
 std::vector<pthread_t> g_extern_p_thds;
+
+#if EXTERN_CC_IMPL == EXTERN_CC_DUMMY
+/* Dummy implementation of register_txn (calls commit immediately).
+ * This is equivalent to no concurrency control. */
+
+void register_txn(txn_man* m_txn, base_query* m_query, row_t* reads[],
+                  row_t* writes[], size_t num_reads, size_t num_writes) {
+    if (WORKLOAD == TEST) {
+        if (g_test_case == READ_WRITE) {
+            ((TestTxnMan*)m_txn)->commit_txn(g_test_case, 0, reads, writes);
+            ((TestTxnMan*)m_txn)->commit_txn(g_test_case, 1, reads, writes);
+            printf("READ_WRITE TEST PASSED\n");
+        } else if (g_test_case == CONFLICT) {
+            ((TestTxnMan*)m_txn)->commit_txn(g_test_case, 0, reads, writes);
+        }
+    } else {
+        m_txn->commit_txn(m_query, reads, writes);
+    }
+    delete[] reads;
+    delete[] writes;
+}
+
+#elif EXTERN_CC_IMPL == EXTERN_CC_GL
+/* Simple implementation of register_txn (uses single global lock). */
+
 pthread_mutex_t g_extern_thread_lock;
 
 void register_txn(txn_man* m_txn, base_query* m_query, row_t* reads[], row_t* writes[],
